d5ff306857516922446c7154ca39c695fe0173ae
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-02-08 20:49:24 +0630

    feat: make no button float around

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello, World!')
+const yesBtn = document.querySelector(".yes__btn");
+const noBtn = document.querySelector(".no__btn");
+const themeSound = new Audio("./sounds/main.mp3");
+
+noBtn.addEventListener("click", () => {
+  themeSound.play();
+  themeSound.loop = true;
+  noBtn.style.position = "absolute";
+  let i = Math.floor(Math.random() * 500) + 1;
+  let j = Math.floor(Math.random() * 500) + 1;
+  noBtn.style.top = `${i}px`;
+  noBtn.style.left = `${j}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+  location.href = "./yes.html";
+});
